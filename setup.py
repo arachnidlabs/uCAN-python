df208575d06d997a1e133d78d0186a0f5cc9ff17
Nick Johnson <arachnid@notdot.net>
2013-11-30 21:00:52 +0000

    Fixed config deps

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name='uCAN',
-	  version='1.0',
-	  description='Python uCAN library',
-	  author='Nick Johnson',
-	  author_email='nick@notdot.net',
-	  url='http://www.arachnidlabs.com/',
-	  license='MIT',
-	  packages=['uCAN'],
-	  dependency_links=['https://bitbucket.org/hardbyte/python-can/downloads/python-can-1.3.tar.gz'],
-	  install_requires=['python-can', 'enum34'],
-	  test_suite='uCAN.tests')
+      version='1.0',
+      description='Python uCAN library',
+      author='Nick Johnson',
+      author_email='nick@notdot.net',
+      url='http://www.arachnidlabs.com/',
+      license='MIT',
+      packages=['uCAN'],
+      dependency_links=['https://bitbucket.org/hardbyte/python-can/get/default.tar.gz'],
+      install_requires=['python-can', 'enum34', 'bitstring'],
+      test_suite='uCAN.tests')
